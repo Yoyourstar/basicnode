8631589f085a8eb9bc4c34e131cacc794a420120
Yoyourstar <diohidian@gmail.com>
2023-09-14 19:32:33 +0700

    tahap1

---
+++ index.js
@@ -0,0 +1,10 @@
+const MamaliaController = require("./class/controllers/mamalia.controller")
+
+function main() {
+    const mamaliaController = new MamaliaController();
+    mamaliaController.getSuara(); 
+
+}
+
+main()
+   
